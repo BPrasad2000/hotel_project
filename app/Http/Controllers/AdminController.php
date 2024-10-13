ef80dcaa9e0e662262ad5196ad9c9b19fb1578f8
BPrasad2000 <buddhiprasad0784558@gmail.com>
2024-10-13 14:33:29 +0530

    display rooms

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,7 +18,8 @@ class AdminController extends Controller
 
             if($usertype == 'user')
             {
-                return view('home.index');
+                $room =Room::all();
+                return view('home.index',compact('room'));
             }
             else if($usertype == 'admin')
             {
@@ -33,7 +34,8 @@ class AdminController extends Controller
     }
 
     public function home(){
-        return view('home.index');
+        $room =Room::all();
+        return view('home.index',compact('room'));
     }
 
     public function create_room(){
